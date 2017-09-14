d01fcf0a7bc4b68b024cf25197e755de69c5e955
Ege Bilecen <egebilecen@icloud.com>
2017-09-14 10:13:57 +0300

    Base files.

---
+++ js/functions.js
@@ -0,0 +1,162 @@
+//=============================================================================
+// Add New Tileset
+//=============================================================================
+function addNewTileset(){
+    var regex = {
+        url : /(ftp|http|https):\/\/(\w+:{0,1}\w*@)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%@!\-\/]))?/,
+        imageUrl : /\.(jpeg|jpg|gif|png)$/
+    };
+    var url = prompt("Lütfen resim URL'sini giriniz:");
+    if( url === null ) return false;
+    if( !regex.url.test(url) )
+    {
+        alert("!!! - Lütfen doğru bir URL giriniz.");
+        return false;
+    }
+    else
+    {
+        if( !regex.imageUrl.test(url) )
+        {
+            alert("!!! - Lütfen doğru bir resim URL'i giriniz.");
+            return false;
+        }
+        else
+        {
+            var oZJCo = decodeURIComponent(url).split("/");
+            var aWani = oZJCo[oZJCo.length - 1].split(".");
+            var imgName = aWani.splice(0,(aWani.length - 1)).join("_").replace("-","_");
+            var imgId   = String(Math.random()).split(".")[1];
+
+            TILESET_LIST[imgId] = {
+                id   : imgId, 
+                name : imgName
+            };
+            
+            var dom = "<li data-id='"+imgId+"'><span id='tileset-name'>"+imgName+" <span style='color:#aaa;'>(Yükleniyor)</span></span></li>";
+            $("#list > ul#tileset-list").append(dom);
+
+            var img  = new Image();
+            img.src  = url;
+            img.setAttribute("data-name",imgName);
+            img.setAttribute("data-id",imgId);
+            img.onload = function(){
+                var id = this.getAttribute("data-id");
+                var imgName = this.getAttribute("data-name");
+                TILESET_LIST[id].img = img;
+                $("li[data-id='"+id+"'] > span#tileset-name").html(imgName).attr("onclick","selectTileset(this)").append("<img class='delete' src='img/substract.png' title='Delete tileset'> <span id='isSelected' data-id='"+id+"' is-selected='false'></span>");
+            }
+        }
+    }
+}
+
+//=============================================================================
+// Select Tileset
+//=============================================================================
+function selectTileset(elem){
+    if( SELECTED_TILESET !== null && SELECTED_TILESET.hasOwnProperty("id") )
+    {
+        var lastId = SELECTED_TILESET.id;
+        $("span#isSelected[data-id='"+lastId+"']").attr("is-selected","false");
+    }
+    elem   = elem.parentNode;
+    var id = elem.getAttribute("data-id");
+    SELECTED_TILESET = TILESET_LIST[id];
+    $("span#isSelected[data-id='"+id+"']").attr("is-selected","true");
+
+    runDrawing(ctx);
+}
+
+//=============================================================================
+// Resize Canvas
+//=============================================================================
+function resizeCanvas(){
+    var width  = $("input[name='c_width']").val();
+    var height = $("input[name='c_height']").val();
+    if( !width || !height )
+    {
+        alert("Lütfen bütün yerleri doldurunuz.");
+        return false;
+    }
+    else
+    {
+        $("canvas#canvas")[0].width  = width;
+        $("canvas#canvas")[0].height = height;
+    }
+}
+
+//=============================================================================
+// Get Settings
+//=============================================================================
+function getSettings(){
+    return {
+        tileset : {
+            width : parseInt($("input[name='tileset_width']").val()),
+            height: parseInt($("input[name='tileset_height']").val())
+        }
+    };
+}
+
+//=============================================================================
+// Draw tileset to canvas
+//=============================================================================
+function runDrawing(ctx,fps){
+    if(typeof fps === "undefined") fps = 60;
+    clearInterval(tilesetInterval);
+    if( SELECTED_TILESET.img.width > ctx.canvas.width || SELECTED_TILESET.img.height > ctx.canvas.height )
+    {
+        var c = confirm("Ekranda gösterilecek resim canvas boyutundan büyük. Canvası yeniden boyutlandıralım mı?");
+        if( c )
+        {
+            ctx.canvas.width  = SELECTED_TILESET.img.width;
+            ctx.canvas.height = SELECTED_TILESET.img.height;
+            updateCanvasSizeIndicator(); 
+        }
+    }
+    //better pixel art graphics settings
+    ctx.imageSmoothingEnabled    = false;
+    ctx.msImageSmoothingEnabled  = false;
+    ctx.mozImageSmoothingEnabled = false;
+
+    var tilesetInterval = setInterval(function(){
+        ctx.clearRect(0,0,ctx.canvas.width,ctx.canvas.height);
+        ctx.drawImage(SELECTED_TILESET.img,0,0);
+
+        //draw grids
+        var settings = getSettings();
+        var row = SELECTED_TILESET.img.width / settings.tileset.width;
+        var col = SELECTED_TILESET.img.height / settings.tileset.height;
+        var gridColor = $("input[name='grid_color']").val();
+        
+        if( row % 1 !== 0 || col % 1 !== 0  )
+        {
+            alert("Tileset'in boyutu "+settings.tileset.width+"x"+settings.tileset.height+" boyutlarını desteklememektedir. Bu yüzden grid(ızgara) çizilemiyor.");
+            clearInterval(tilesetInterval);
+            return false;
+        }
+
+        for( var c=0; c < col; c++ )
+        {
+            for( var r=0; r < row; r++ )
+            {
+                ctx.beginPath();
+                ctx.strokeStyle = gridColor;
+                //horizontal line
+                ctx.moveTo(r * settings.tileset.width, c * settings.tileset.height);
+                ctx.lineTo(r * settings.tileset.width + settings.tileset.width, c * settings.tileset.height);
+                //vertical line
+                ctx.moveTo(r * settings.tileset.width, c * settings.tileset.height);
+                ctx.lineTo(r * settings.tileset.width, c * settings.tileset.height + settings.tileset.height);
+                ctx.stroke();
+                ctx.closePath();
+            }
+        }
+    }, 1000 / fps);
+}
+
+//=============================================================================
+// Update canvas size indicator inputs
+//=============================================================================
+function updateCanvasSizeIndicator(){
+    $("input[name='c_width']").val(ctx.canvas.width);
+    $("input[name='c_height']").val(ctx.canvas.height);
+}
---
+++ js/init.js
@@ -0,0 +1,4 @@
+var TILESET_LIST = {};
+var SELECTED_TILESET = null;
+console.log("https://images.duckduckgo.com/iu/?u=https%3A%2F%2Fwww.sitepoint.com%2Fwp-content%2Fthemes%2Fsitepoint%2Fassets%2Fimages%2Ficon.javascript.png");
+console.log("https://i.hizliresim.com/RO6Boa.png");
