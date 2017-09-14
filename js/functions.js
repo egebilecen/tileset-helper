c64d0be0b1f542fe2533d6d9e628898b04a2529e
Ege Bilecen <egebilecen@icloud.com>
2017-09-14 19:31:20 +0300

    improved

--- js/functions.js
+++ js/functions.js
@@ -47,12 +47,7 @@ function addNewTileset(){
                 $("li[data-id='"+id+"'] > span#tileset-name").html(imgName).attr("onclick","selectTileset(this)").append(" <span id='isSelected' data-id='"+id+"' is-selected='false'></span>").parent().append("<img class='delete' src='img/substract.png' title='Delete tileset' onclick='deleteTileset(\""+id+"\")'>");
             
                 //set first gid and last gid
-                var firstGid = MEMORY.lastGid + 1;
-                var lastGid  = (this.width / settings.tileset.width) * (this.height / settings.tileset.height) + MEMORY.lastGid;
-                TILESET_LIST[id].firstGid = firstGid;
-                TILESET_LIST[id].lastGid  = lastGid;
-
-                MEMORY.lastGid = lastGid;
+                setTilesetGid(id,this.width,this.height)
             }
         }
     }
@@ -81,6 +76,15 @@ function deleteTileset(id){
     if( !TILESET_LIST.hasOwnProperty(id) ) return false;
     delete TILESET_LIST[id];
     $("li[data-id='"+id+"']").remove();
+
+    //reset tileset gid's
+    MEMORY.lastGid = 0;
+    for( tilesetGid in TILESET_LIST )
+    {
+        if(!TILESET_LIST.hasOwnProperty(tilesetGid)) continue;
+        var tileset = TILESET_LIST[tilesetGid];
+        setTilesetGid(tilesetGid,tileset.img.width,tileset.img.height);
+    }
 }
 
 //=============================================================================
@@ -129,6 +133,33 @@ function runDrawing(ctx,fps){
             updateCanvasSizeIndicator(); 
         }
     }
+
+    //add event listener to canvas
+    ctx.canvas.addEventListener("click",function(e){
+        var settings = getSettings();
+        var x = e.pageX;
+        var y = e.pageY;
+        var positions = findWhichRectangle(x,y);
+
+        var mrDrq = (SELECTED_TILESET.img.width / settings.tileset.width) * positions.height;
+        var Gid = SELECTED_TILESET.firstGid + positions.width + mrDrq;
+
+        setOutput("Tile Informations:","Width: "+positions.width+" | Height: "+positions.height+" <span style='color:#333;'>(Indexes)</span><br/>Tile Gid:"+Gid+"<br/><br/>Tile: <span style='color:#333;'>(Overview)</span><br/><canvas id='output-overview' width='"+settings.tileset.width+"' height='"+settings.tileset.height+"' style='border:1px solid black;'></canvas>", function(positions){
+            var settings = getSettings();
+            var _ctx = document.getElementById("output-overview").getContext("2d");
+            _ctx.clearRect(0,0,settings.tileset.width,settings.tileset.height);
+            _ctx.beginPath();
+            _ctx.drawImage(
+                SELECTED_TILESET.img,
+                positions.width * settings.tileset.width, positions.height * settings.tileset.height,
+                settings.tileset.width, settings.tileset.height,
+                0,0,
+                settings.tileset.width, settings.tileset.height
+            );
+            _ctx.closePath();
+        }, positions);
+    });
+
     //better pixel art graphics settings
     ctx.imageSmoothingEnabled    = false;
     ctx.msImageSmoothingEnabled  = false;
@@ -169,11 +200,46 @@ function runDrawing(ctx,fps){
         }
     }, 1000 / fps);
 }
+//=============================================================================
+// Set tileset gid
+//=============================================================================
+function setTilesetGid(id,imgWidth,imgHeight){
+    if(!TILESET_LIST.hasOwnProperty(id)) return false;
 
+    var settings = getSettings();
+    var firstGid = MEMORY.lastGid + 1;
+    var lastGid  = (imgWidth / settings.tileset.width) * (imgHeight / settings.tileset.height) + MEMORY.lastGid;
+    TILESET_LIST[id].firstGid = firstGid;
+    TILESET_LIST[id].lastGid  = lastGid;
+    MEMORY.lastGid = lastGid;
+    return true;
+}
 //=============================================================================
 // Update canvas size indicator inputs
 //=============================================================================
 function updateCanvasSizeIndicator(){
     $("input[name='c_width']").val(ctx.canvas.width);
     $("input[name='c_height']").val(ctx.canvas.height);
+}
+
+//=============================================================================
+// Find tile location
+//=============================================================================
+function findWhichRectangle(x,y){
+    var settings = getSettings();
+    w = Math.ceil(x / settings.tileset.width) - 1;
+    h = Math.ceil(y / settings.tileset.height) - 1;
+
+    return { width : w, height : h }
+}
+
+//=============================================================================
+// Set output section's things
+//=============================================================================
+function setOutput(title,content, func, params){
+    if(typeof title === "undefined" || typeof title !== "string")     title = "Data Output:";
+    if(typeof content === "undefined" || typeof content !== "string") content = "-";
+    $("div#info > span#title").html(title);
+    $("div#info > span#data").html(content);
+    if(typeof func === "function") func.call(false,params);
 }
